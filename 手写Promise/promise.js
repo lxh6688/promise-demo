476ff72054d913d8060998c9e6f7bf00881ef4ed
lxh6688 <1272662790@qq.comom>
2021-02-10 16:39:59 +0800

    resolve 与 reject

--- 手写Promise/promise.js
+++ 手写Promise/promise.js
@@ -1,6 +1,16 @@
 //声明构造函数
-function Promise(){
+function Promise(executor){
+  //resolve 函数
+  function resolve(data){
 
+  }
+  //reject 函数
+  function reject(data){
+    
+  }
+
+  //同步调用 [执行器函数]
+  executor(resolve,reject)
 }
 
 //添加 then 方法
