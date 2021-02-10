aec4cc4e348869f3c5c229cddd2e924cfa3d4faa
lxh6688 <1272662790@qq.comom>
2021-02-10 17:06:03 +0800

    resolve 与 reject代码实现

--- 手写Promise/promise.js
+++ 手写Promise/promise.js
@@ -1,12 +1,21 @@
 //声明构造函数
 function Promise(executor){
+  this.PromiseState = 'pending'
+  this.PromiseResult = null
+
+  const self = this
+
   //resolve 函数
   function resolve(data){
-
+    // 1. 修改对象的状态 (promiseState)
+    self.PromiseState = 'fulfilled' //resolved
+    // 2. 设置对象结果值 (promiseResult)
+    self.PromiseResult = data
   }
   //reject 函数
   function reject(data){
-    
+    self.PromiseState = 'rejected'
+    self.PromiseResult = data
   }
 
   //同步调用 [执行器函数]
